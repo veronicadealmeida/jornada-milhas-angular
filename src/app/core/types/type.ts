2a505944cc78dc19d05208af8ddf709f2f8dfcae
almeida.veronica <almeida.vrc@gmail.com>
2024-01-31 09:46:58 -0300

    refactory do projeto inicial

---
+++ src/app/core/types/type.ts
@@ -0,0 +1,12 @@
+export interface Promocao {
+    id: number
+    destino: string
+    imagem: string
+    preco: number
+}
+
+export interface UnidadeFederativa {
+    id: number;
+    nome: string;
+    sigla: string;
+}
